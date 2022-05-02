df94d312a74ca3a351ddec8f3a4b1698525e2a30
SunshineZerg <8208201214@csu.edu.com>
2022-05-03 02:16:00 +0800

    update leave/index

--- src/domain/base.ts
+++ src/domain/base.ts
@@ -5,5 +5,5 @@ type imgBase64string = string;
 type baseResponse = { msg: string, code: number };
 type htttpRequest = <response = baseResponse,query = void,form = void>(url:string,q?:query,f?:form)=> Promise<response>;
 type phonenumber = string;
-
-export { phonenumber, uuid, timestamp, imguri, imgBase64string, baseResponse, htttpRequest };
+type date = { year: string, month:string ,day:string }
+export { phonenumber, uuid, timestamp, imguri, imgBase64string, baseResponse, htttpRequest, date};
--- src/domain/business/leave/index.ts
+++ src/domain/business/leave/index.ts
@@ -1,15 +1,35 @@
 import { uuid } from "../../base"
-import { phonenumber } from "@/domain/base"
+import { phonenumber , date } from "@/domain/base"
 
 export type LeaveApplication = {
     id: uuid
-    phonenumber: phonenumber
-    emergencyPerson: string
-    emergencyContact: phonenumber
-    place: string // 外出地点
-    startTime: Date
-    endTime: Date
-    reason: string
+    leaveType: number //此条请假请求状态
+    applicantStuID: string
+    applicantStuName: string
+    leaveStartTime: string
+    leaveEndTime: string
+    leaveReason: string
+    status: number
+}
+export type DetailedLeaveApplication = {
+    id: uuid
+    leaveType: number
+    applicantStuID: string
+    applicantStuName: string
+    phoneNumber: phonenumber
+    emergencyName: string
+    emergencyPhoneNumber: phonenumber
+    targetPlace: string
+    leaveStartTime: date
+    leaveEndTime: date
+    leaveReason: string
+    status: number
+    denyReason: string
+    monitorStuID: String
+    InstructorInsID: string
+    applyTime: date
+    monitorHandleTime: date
+    instructorHandleTime: date
 }
 const Leaveapplies:LeaveApplication[] = [];
 //获取请假列表的请求参数
