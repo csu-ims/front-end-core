28148034bcd5b455438c1c9f6fa648983e18a2d8
SunshineZerg <8208201214@csu.edu.com>
2022-05-02 14:29:16 +0800

    push test

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,6 @@
 import { HttpFoundation } from "@/foundations";
 import { Container } from "typescript-ioc";
 
-const http = ()=>Container.get(HttpFoundation);
+const http = () =>Container.get(HttpFoundation);
 
 export { http }
