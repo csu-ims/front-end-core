0f246f4db92dde3393a41aa2d78f3266a5dba3da
rick lin <8208200137@csu.edu.cn>
2022-08-25 17:04:10 +0800

    refact:--20220826 convert throwing error to returning error code.

---
+++ src/utils/net.ts
@@ -0,0 +1,3 @@
+import { errorCode } from "@/types/export";
+
+export const netErrorToErrorCode = (e:Error):errorCode => { console.debug(`[useLogin.ts/login] request fail at: ${e.message}`) ; return 'NET'};
